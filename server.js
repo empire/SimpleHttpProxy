faec43da3e98afb47a06e59197d8ed4541225b6c
Hossein Zolfi <hossein.zolfi@gmail.com>
2015-05-25 07:48:08 +0430

    Parse client data on server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var net = require('net');
+var request = require('./lib/request');
+
 var server = net.createServer(function(c) { //'connection' listener
   console.log('client connected');
   c.on('end', function() {
     console.log('client disconnected');
   });
-  c.write('hello\r\n');
-  c.pipe(c);
+  c.on('data', function(data) {
+    var req = request.createRequestByRequestMessage(data + '');
+    console.log(req)
+  })
 });
 server.listen(4567, function() { //'listening' listener
   console.log('server bound');
